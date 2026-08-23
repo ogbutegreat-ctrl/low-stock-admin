9756e0a32e33ebaba114720dd024b54c6d1e4815
ogbutegreat-ctrl <ogbutegreat@gmail.com>
2026-08-23 23:54:03 +0100

    setting.py for hosting

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--0or8p0c@ab+9ru9r3wu!t3ykg+k4+31we9!ptscjnb^@e%3rn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Ellamanuel.pythonanywhere.com']
 
 
 # Application definition
@@ -116,6 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "/static/"
 
 
